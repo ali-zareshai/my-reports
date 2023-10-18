0d7ead1ba7eedd79cf362b32727f408ae651bc43
ali zareshahi <alititi96@gmail.com>
2023-10-18 23:30:50 +0330

    add font vazir

--- app/src/main/java/com/zareshahi/myreport/ui/theme/Type.kt
+++ app/src/main/java/com/zareshahi/myreport/ui/theme/Type.kt
@@ -2,33 +2,40 @@ package com.zareshahi.myreport.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.zareshahi.myreport.R
 
+val vazirFamily = FontFamily(
+    Font(R.font.vazir_thin, FontWeight.Thin),
+    Font(R.font.vazir, FontWeight.Normal),
+    Font(R.font.vazir_bold, FontWeight.Bold),
+    Font(R.font.vazir_medium, FontWeight.Medium),
+    Font(R.font.vazir_light, FontWeight.Light),
+)
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = vazirFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+    ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = vazirFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = vazirFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 11.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
     )
-    */
 )
